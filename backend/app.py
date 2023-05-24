0ec3ef486891127e38df25ee6275778586299c9b
Trevor Fagan <trevorfagan77@gmail.com>
2023-05-24 11:18:54 -0400

    New prod build (#21)
    * Added CORS headers
    
    * Updated origin url
    
    * Updated to use production ready build
    
    * Fixed linting issues
    
    * Fixed linting issue

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 from flask_cors import CORS
 
 load_dotenv()
-app = Flask(__name__)
-CORS(app, origins='http://www.coffee-shop-coders.com')
+app = Flask(
+    __name__,
+    static_folder='frontend-build/static',
+    template_folder='frontend-build'
+)
+CORS(app, origins='http://www.coffeeshopcoders.com')
 
 
 # default route for nows
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
+
+
+@app.route('/')
+def main_page():
+    return render_template("index.html")
+
+
 @app.route('/api')
 def hello_world():
     return 'Hello, World!'
